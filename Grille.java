6ec0feb251bce86869ef158ee7494dc988589959
tdumond <thomas@PC_FIXE_THOMAS.localdomain>
2022-05-01 23:56:24 +0200

    jouable partiellement

--- Grille.java
+++ Grille.java
@@ -1,5 +1,5 @@
 public class Grille {
-    Pions[][] cases;
+    Pions[][] cases = new Pions[6][7];
 
     // Constructeur
     Grille() {
@@ -29,13 +29,53 @@ public class Grille {
                 } else {
                     System.out.print(" ");
                 }
-                System.out.print("|\n");
             }
-        }
-        for (int k = 0; k < 7; k++) {
-            System.out.print(" - ");
+            System.out.print("|\n");
         }
         System.out.println();
+    }
+
+    public boolean patie_finie() {
+        // Check for horizontal
+        for (int i = 0; i < 6; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (cases[i][j] != null && cases[i][j + 1] != null && cases[i][j + 2] != null && cases[i][j + 3] != null
+                        && cases[i][j].couleur == cases[i][j + 1].couleur
+                        && cases[i][j].couleur == cases[i][j + 2].couleur
+                        && cases[i][j].couleur == cases[i][j + 3].couleur) {
+                    System.out.println("Joueur " + cases[i][j].couleur + " a gagné");
+                    return true;
+                }
+            }
+        }
+
+        // Check for vertical
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 7; j++) {
+                if (cases[i][j] != null && cases[i + 1][j] != null && cases[i + 2][j] != null && cases[i + 3][j] != null
+                        && cases[i][j].couleur == cases[i + 1][j].couleur
+                        && cases[i][j].couleur == cases[i + 2][j].couleur
+                        && cases[i][j].couleur == cases[i + 3][j].couleur) {
+                    System.out.println("Joueur " + cases[i][j].couleur + " a gagné");
+                    return true;
+                }
+            }
+        }
+
+        // Check for diagonal
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 4; j++) {
+                if (cases[i][j] != null && cases[i + 1][j + 1] != null && cases[i + 2][j + 2] != null
+                        && cases[i + 3][j + 3] != null && cases[i][j].couleur == cases[i + 1][j + 1].couleur
+                        && cases[i][j].couleur == cases[i + 2][j + 2].couleur
+                        && cases[i][j].couleur == cases[i + 3][j + 3].couleur) {
+                    System.out.println("Joueur " + cases[i][j].couleur + " a gagné");
+                    return true;
+                }
+            }
+        }
+
+        return false;
     }
 
 }
--- Main.java
+++ Main.java
@@ -1,28 +1,39 @@
 public class Main {
 
     public static void main(String[] args) {
-        Couleur couleur_a_jouer=Couleur.Jaune;
-        Grille grille=new Grille();
-        boolean Joueur1=true;
-        int colonne_a_jouer=0;
-        boolean fin_de_partie=false;
+        Couleur couleur_a_jouer = Couleur.Jaune;
+        Grille grille = new Grille();
+        boolean Joueur1 = true;
+        int colonne_a_jouer = 0;
+        boolean fin_de_partie = false;
 
         grille.afficher();
 
-        while(!fin_de_partie){
-            if(Joueur1){
+        while (!fin_de_partie) {
+            if (Joueur1) {
                 System.out.println("Joueur 1, quelle colonne voulez-vous jouer ?");
-                colonne_a_jouer = Integer.parseInt(System.console().readLine());
+                colonne_a_jouer = Integer.parseInt(System.console().readLine()) - 1;
+                if (colonne_a_jouer < 0 || colonne_a_jouer > 6) {
+                    System.out.println("Vous devez entrer un nombre entre 1 et 7");
+                    continue;
+                }
                 grille.AjouterPion(couleur_a_jouer, colonne_a_jouer);
                 grille.afficher();
-                Joueur1=false;
-            }
-            else{
+                couleur_a_jouer = Couleur.Rouge;
+                Joueur1 = false;
+                fin_de_partie = grille.patie_finie();
+            } else {
                 System.out.println("Joueur 2, quelle colonne voulez-vous jouer ?");
-                colonne_a_jouer = Integer.parseInt(System.console().readLine());
+                colonne_a_jouer = Integer.parseInt(System.console().readLine()) - 1;
+                if (colonne_a_jouer < 0 || colonne_a_jouer > 6) {
+                    System.out.println("Vous devez entrer un nombre entre 1 et 7");
+                    continue;
+                }
                 grille.AjouterPion(couleur_a_jouer, colonne_a_jouer);
                 grille.afficher();
-                Joueur1=true;
+                couleur_a_jouer = Couleur.Jaune;
+                Joueur1 = true;
+                fin_de_partie = grille.patie_finie();
             }
         }
 
